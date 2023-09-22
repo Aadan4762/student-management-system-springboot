75c3c9ee8d7ceaf1067013152e3c8a07fce656d6
adesh <aadan4762@gmail.com>
2023-09-22 11:32:45 +0300

    initial

---
+++ src/main/java/com/adan/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.adan.repository;
+
+import com.adan.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
